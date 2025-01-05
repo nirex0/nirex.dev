827f6a81cf5c7671d56df1e1e8e0f3dc3ec8005d
nirex0 <nirex.0@gmail.com>
2025-01-05 13:18:57 +0330

    GH Deployment @ 13:18:57

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default ({
+  target: 'static',
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
